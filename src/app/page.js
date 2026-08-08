70954a906c8dab9b1a4af94b1d088c04f1eacf5e
adnannawawi <adnannawawi1301@gmail.cpm>
2026-08-08 22:34:07 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -164,7 +164,7 @@ export default function Home() {
         "Nama_Jabatan", 
         "Jabatan", 
         "Petugas", 
-        "Petugas Ukur"
+        "Petugas Ukur",
       ]);
 
       let fullNoBerkas = formatValue(nomor);
