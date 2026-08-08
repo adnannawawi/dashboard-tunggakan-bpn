f3baef6a036d0dda5d5069ea58a2015338207279
adnannawawi <adnannawawi1301@gmail.cpm>
2026-08-08 21:02:18 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>dashboard-app.com</title>
+        <title>dashboard-app</title>
       </head>
       <body>{children}</body>
     </html>
